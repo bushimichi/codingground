54c3f9942798e42896c55c15b09933ff7d5fdfab
bushimichi <bushimichi@gmail.com>
2017-01-08 16:23:19 +0900

    Created by Coding Ground Client

---
+++ python-test/for.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+print "for.py"
+
+
+for num in range(100):
+    if num % 10:
+        continue
+    
+    print num
+
+
+test_list = ['い','ろ','は','に','ほ','へ','と']
+for val in test_list:
+    print val
